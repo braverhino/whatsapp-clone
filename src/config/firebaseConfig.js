437f207b1f2cf11dafa6c4b285294f2f84d52603
bravelhino <saparov20003@mail.ru>
2021-08-12 13:07:34 +0600

    changed

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,9 +1,3 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyDHY5ytxC4MM5E6s353X6RMMxR69sMXO_Y",
-  authDomain: "fir-practise-871d4.firebaseapp.com",
-  projectId: "fir-practise-871d4",
-  storageBucket: "fir-practise-871d4.appspot.com",
-  messagingSenderId: "236820653435",
-  appId: "1:236820653435:web:5039ce580fb620830949de",
-  measurementId: "G-82JST2ZHCC"
+ // firebse config
 }
